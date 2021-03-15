6b7dc233978438a37d17c67f0cd134ab6b37eec3
Claire Luning <cnluning@gmail.com>
2021-03-15 10:48:45 -0500

    remove image from home page

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,12 +1,5 @@
 import html from "html-literal";
 
 export default () => html`
-  <img
-    src="https://images.pexels.com/photos/235990/pexels-photo-235990.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    height="200px"
-    width="em"
-    alt="Cairn Stones"
-    class="center"
-  />
   <p>Through 30 days of consistent journaling, you can create better habits.</p>
 `;

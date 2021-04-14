314bea0a71498e502f7e04402144e57adae04a01
Claire Luning <cnluning@gmail.com>
2021-04-13 22:16:35 -0500

    test backend

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  Name: String,
-  Email: String,
-  Password: String
-});
+// const userSchema = new mongoose.Schema({
+//   Name: String,
+//   Email: String,
+//   Password: String
+// });
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-module.exports = {
-  model: User,
-  Schema: userSchema
-};
+// module.exports = {
+//   model: User,
+//   Schema: userSchema
+// };
